aef4f2e871b4549e2ed13105621c2e9d4ea8b6b2
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-07 07:38:24 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='.')
 
 @bot.command()
-async def about(ctx)
+async def about():
     embed=discord.Embed(title="About me", description="This will list everyone that helped!", color=0x4ffd1c)
     embed.add_field(name=Author/Papi/Creator, value=XXWolfBane#5559, inline=False)
     await self.bot.say(embed=embed)
