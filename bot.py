95960b4a94a4f66877a6212833cb43d8545b5c8f
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-07 07:32:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ bot = commands.Bot(command_prefix='.')
 @bot.command()
 async def about(ctx):
      embed = discord.Embed(title="B-Day Bot!", description="Hello, this is the about command of B-Day Bot!"
-     embed.add_field(title="Author/Creater/Papi" value="XXWolfBane#5559")
+     embed.add_field(title="Author/Creater/Papi", value="XXWolfBane#5559")
+
      await ctx.send(embed=embed)
                            
 bot.run("NDY1MTEzNzAwNzIyODAyNjk4.DiIyaQ.oaEeUImFeCIQBI5n3S1eMNnLofI")
